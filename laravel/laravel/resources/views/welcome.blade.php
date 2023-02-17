a1c38256c574398bcd12adbd2463121a5d5ebb83
KayoMartins2405 <martinslima2424@gmail.com>
2023-02-16 22:36:59 -0300

    aula02pwi-3°módulo

---
+++ laravel/laravel/resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <h1>Como executar um projeto Laravel</h1>
+
+    <nav>
+            <ul>
+            <li><p>Para começarmos, primeiramente devemos abrir o CMD da máquina</p></li>
+            <li><p>Crie um diretório usando <code>mkdir</code> e o nome da pasta</p></li>
+            <li><p>Use o comando <code>cd</code> e o nome da pasta para acessar o diretório</p></li>
+            <li><p>Instale o composer, caso já instalado confira colocando no cmd o código <code>composer -v</code></p></li>
+            <li><p>Logo em seguida, use o código <code> composer create-project "nome da pasta" </code></p></li>
+            <li><p>Use novamente o cd com o nome da pasta utilizada, exemplo: <code>cd "nome da pasta"</code> </p></li>
+            <li><p>Após isso, utilize o atalho <code>code .</code></p></li>
+            <li><p>Com o atalhado digitado, vc será realocado automaticamente para o VS CODE </p></li>
+            <li><p>Na parte superior do VS CODE, clique em "Terminal" e coloque o seguinte comando: php artisan serve(executa o projeto) </p></li>
+            <li><p>Abra o navegador e digite localhost:8000</p></li>
+
+
+</ul>
+    </nav>
+
+
+</body>
+</html>
